a62b4faa3f4ef52c88a005757069ece8ab7e5501
HurricaneTBS <634092350@qq.com>
2021-08-09 11:23:33 +0800

    push

--- test.js
+++ test.js
@@ -1,14 +1,29 @@
-function timeout(ms){
-    return new Promise((resolve,reject)=>{
-        setTimeout(resolve, ms,'done');
-    })
-}
-
-new Promise((resolve,reject)=>{
-    setTimeout(resolve, ms,'done');
-}).then()
-
-
-class Pro<T>{
-    then()
-}
+const getJSON = function(url) {
+    const promise = new Promise(function(resolve, reject){
+      const handler = function() {
+        if (this.readyState !== 4) {
+          return;
+        }
+        if (this.status === 200) {
+          resolve(this.response);
+        } else {
+          reject(new Error(this.statusText));
+        }
+      };
+      const client = new XMLHttpRequest();
+      client.open("GET", url);
+      client.onreadystatechange = handler;
+      client.responseType = "json";
+      client.setRequestHeader("Accept", "application/json");
+      client.send();
+  
+    });
+  
+    return promise;
+  };
+  
+  getJSON("/posts.json").then(function(json) {
+    console.log('Contents: ' + json);
+  }, function(error) {
+    console.error('出错了', error);
+  });
