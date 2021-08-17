9c92068a7cc028905cbc7ee67011c2795b5fae90
HurricaneTBS <634092350@qq.com>
2021-08-17 15:34:04 +0800

    push

--- readme.md
+++ readme.md
@@ -6,3 +6,17 @@
 
 [IT dongdong](https://docs.itdongdong.com/collection/index.html)
 
+[深入前端核心理论，记录个人博客](https://github.com/simon9124/my_demos)
+
+## 项目
+
+[基于 vue2.0+koa2+mongodb 实现注册登录](https://github.com/stzhongjie/vue-login)
+
+## 文档
+
+[函数式编程入门--阮一峰](https://www.ruanyifeng.com/blog/2017/02/fp-tutorial.html)
+[尾调用优化--阮一峰](http://www.ruanyifeng.com/blog/2015/04/tail-call.html)
+[JavaScript 标准参考教程](http://javascript.ruanyifeng.com/#toc3)
+[typescript 英文文档](https://www.typescriptlang.org/docs/handbook/2/objects.html)
+## mine learn
+
--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-const a = {};
-Object.assign(a, { y: 80, x: 10 });
-
-console.log(a.x,a.y);
+function sayHello(greeting) {
+    return `${greeting}, ${this.name}`;
+  }
+  
+  const user = {
+    name: 'Harrison',
+  };
+  
+  // using call
+  console.log(sayHello.call(user, 'Hello')); // prints "Hello, Harrison"
+  
+  // using apply
+  console.log(sayHello.apply(user, ['Good morning'])); // prints "Good morning, Harrison"
+  
+  // using bind
+  const boundSayHello = sayHello.bind(user, 'Hey there');
+  console.log(sayHello.bind(user, 'Hey there')); // prints "Hey there, Harrison"
