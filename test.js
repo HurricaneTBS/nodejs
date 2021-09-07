d6b6aa4f8437e8f4278845c173fc99ce12645969
HurricaneTBS <dreamfightter@sina.com>
2021-09-07 11:46:44 +0800

    push

--- test.js
+++ test.js
@@ -1,6 +1,33 @@
-function escapeRegExp(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-  }
+function sendLog(status) {
+  console.log(`log message is ${status}`);
+}
 
+function jumpTo(pageUrl) {
+  console.log(`跳转到 ${pageUrl}`);
+}
 
- console.log( escapeRegExp("[test]苏军杨的工厂"));
+const actions = new Map([
+  [
+    { identity: "guest", status: 1 },
+    () => {
+      /*do sth*/
+    },
+  ],
+  [
+    { identity: "guest", status: 2 },
+    () => {
+      /*do sth*/
+    },
+  ],
+  //...
+]);
+
+const onButtonClick = (identity, status) => {
+  let action = [...actions].filter(
+    ([key, value]) => key.identity == identity && key.status == status
+  );
+  console.log(action);
+  action.forEach(([key, value]) => value.call(this));
+};
+
+onButtonClick("guest",1)
