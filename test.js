433140398b6a76f7840dbf94f756749788c10187
HurricaneTBS <634092350@qq.com>
2021-09-15 17:58:47 +0800

    push

--- test.js
+++ test.js
@@ -1,18 +1,30 @@
+const synchronousFile = (id) => {
+  console.log(`开始同步文件，id为：${id}`);
+};
 
-const MyImage = (function () {
-  const imageNode = new Image(200, 100);
-  document.body.appendChild(imageNode);
-  const img = new Image();
-  img.onload = function () {
-    imageNode.src = img.src;
-  };
-  return {
-    setSrc(src) {
-      imageNode.src =
-        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F201710%2F23%2F20171023160752_PBGwi.gif&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1634281117&t=4a9b3c4bfb52442efb23a1745adfca2e";
-      img.src = src;
-    },
+const proxySynchronousFile = (() => {
+  const cache = [];
+  let timer = null;
+  return function (id) {
+    cache.push(id);
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      synchronousFile(cache.join(","));
+      clearTimeout(timer);
+      timer = null;
+      cache.length = 0;
+    }, 2000);
   };
 })();
 
-MyImage.setSrc("https://pics7.baidu.com/feed/8694a4c27d1ed21b3470873872dbc3cd53da3ff4.png?token=eb83c87627ca445b920733b3b9740f23")
+const checkboxes = document.getElementsByTagName("input");
+
+for (let i = 0, c; (c = checkboxes[i++]); ) {
+  c.onclick = function () {
+    if (this.checked === true) {
+        proxySynchronousFile(this.id);
+    }
+  };
+}
