2077299c1e4e85cb03d805bd08be30e2243aca44
HurricaneTBS <634092350@qq.com>
2021-10-26 15:19:54 +0800

    piush

--- person.js
+++ person.js
@@ -1,3 +1,3 @@
-const str = "abc abbc abbbc";
-const res = str.match(/ab{1,5}c/g)
-console.log(res);
+const regex = /a[123]b/g;
+const str = "a0b a1b a2b a3b a4b";
+console.log( str.match(regex) ); // [ 'a1b', 'a2b', 'a3b' ]
