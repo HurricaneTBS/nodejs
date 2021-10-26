117fc454f9a041da2122b40e8fb2a281c3accfbf
HurricaneTBS <634092350@qq.com>
2021-10-26 16:26:45 +0800

    push

--- person.js
+++ person.js
@@ -1,3 +1,4 @@
-const regex = /goodbye|good/g;
-const str = "goodbye";
-console.log(str.match(regex)); // [ 'good' ]
+const regex = /^(0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9])$/
+console.log( regex.test("7:9") ); // true
+console.log( regex.test("23:59") ); // true
+console.log( regex.test("02:07") ); // true
