127690d0e74def4f2bfa28a12ec8348c6d69fc8b
HurricaneTBS <634092350@qq.com>
2021-10-26 17:06:52 +0800

    push

--- person.js
+++ person.js
@@ -1,4 +1,5 @@
-const regex = /^(0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9])$/
-console.log( regex.test("7:9") ); // true
-console.log( regex.test("23:59") ); // true
-console.log( regex.test("02:07") ); // true
+const regex = /^|$/g
+
+const str = "hello  hello";
+
+console.log(str.replace(regex,"--"));
